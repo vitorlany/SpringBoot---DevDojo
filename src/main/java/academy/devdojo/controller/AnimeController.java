18f5594dd96b30aeff254a85fb59aa56dde7382b
Vitor Lany <vitorlany99@gmail.com>
2023-01-17 17:03:13 -0300

    Aula 11/12

--- src/main/java/academy/devdojo/controller/AnimeController.java
+++ src/main/java/academy/devdojo/controller/AnimeController.java
@@ -31,4 +31,16 @@ public class AnimeController {
     public ResponseEntity<Anime> save(@RequestBody Anime anime) {
         return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
     }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        animeService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> replace(@RequestBody Anime anime) {
+        animeService.replace(anime);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
